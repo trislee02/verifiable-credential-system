9a0fbc1bb2f639c7df3d2c08a94b4c1e70e941d2
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-07-08 19:38:19 +0700

    Issue one credential (UI Only)

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import IssuersPage from "./components/Issuers/IssuersPage";
 
 const App = () => {
-    const authenticated = useSelector(store => store.authSlice.isAuthenticated);
+    const authenticated = useSelector((store) => store.authSlice.isAuthenticated);
     if (!authenticated) return <Navigate to="/auth/login" />;
     return (
         <React.Fragment>
             <Header />
-            <h1>Hello World!</h1>
+            <Routes>
+                <Route path="issuers" element={<IssuersPage />} />
+                <Route path="holders" element={<h1>This is Holders page</h1>} />
+                <Route path="verifiers" element={<h1>This is Verifiers page</h1>} />
+            </Routes>
         </React.Fragment>
     );
-}
+};
 
 export default App;
